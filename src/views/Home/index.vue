e8abd8a224da434417ab4a72067ec5f51ad17d99
zhangyueqingyun <zhangyueqingyun@foxmail.com>
2022-08-27 12:31:28 +0800

    feat(Home): add tab of navigator

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -7,6 +7,11 @@
     <basic-layout>
         <template v-slot:nav>
             <div class="item active">最新发布</div>
+            <div class="item">大前端</div>
+            <div class="item">计算机</div>
+            <div class="item">系统设计</div>
+            <div class="item">项目管理</div>
+            <div class="item">学习日记</div>
         </template>
         <template v-slot>
             <blogs></blogs>
@@ -20,13 +25,16 @@
         border: 2px solid transparent;
         padding: 0 3px;
         cursor: pointer;
+        color: #555;
         transition: color 0.3s;
 
         &:hover {
-            color: #555;
+            color: #000;
         }
     }
+
     .active {
         border-bottom: 2px solid #fd8c73;
+        color: #000;
     }
 </style>
